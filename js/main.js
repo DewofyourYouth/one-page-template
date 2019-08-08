ae36b24d7dd898e0e69aebffa4f7cff8acd14887
Jacob Shore <jacob@perrysysinc.com>
2019-08-08 15:45:33 +0300

    javascript and flexbox fixes

--- js/main.js
+++ js/main.js
@@ -4,19 +4,42 @@ Copyright (c) 2019 Jacob Shore
 
 */
 
+
+
 $(document).ready(function(){
     const menus = $( '#menus' );
     const cm = $( '#collapse-menu' );
+    const header = $('header');
 
-    if ($(window).width() < 1200) {
+    // On laptops and desktops, make nav fixed on scrolling
+    if ($(window).width() > 1000) {
+        $(window).scroll(function(){
+            if(window.scrollY > 50) {
+                header.addClass('fixed-top');
+                $('#header-contact-area').hide();
+                $('#logo-area').addClass('logo-scrolled', 1000);
+            } else {
+                header.removeClass('fixed-top');
+                $('#header-contact-area').show();
+                $('#logo-area').removeClass('logo-scrolled', 1000);
+            }
+        });
+    }
+
+    // Nav handling for mobile
+
+    if ($(window).width() < 1200) { 
         cm.click(function(){
-            menus.toggle("slow", "linear");
+            menus.toggle("slow", "linear"); // toggle the nav when clicking the collapse menu button
         });
         
         $( 'a' ).click(function(){
-            menus.hide();
+            menus.hide(); // when a link is clicked hide the nav
         });
     }
+
+    // owl carousel params
+    // see docs here: https://owlcarousel2.github.io/OwlCarousel2/index.html
 
     $(".owl-carousel").owlCarousel({
         loop: true,
@@ -43,4 +66,20 @@ $(document).ready(function(){
         ],
     });
 
+
+ 
+
   });
+  
+if ($(window).width() > 1000) {
+ 
+    // this offsets the link for fixed nav-bar
+
+    $('.link').click(function(e){
+        let target = e.target.hash;
+        let divCoords = $(target).offset();
+        e.preventDefault();
+        window.scrollTo(divCoords.left, divCoords.top - 50);
+    });
+
+}
